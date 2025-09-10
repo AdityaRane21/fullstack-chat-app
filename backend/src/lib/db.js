0b14a90b034a45be4ed1df68e6fee840e4524c63
AdityaRane21 <raneaditya354@gmail.com.com>
2025-09-10 13:00:41 +0530

    Project completed

---
+++ backend/src/lib/db.js
@@ -0,0 +1,15 @@
+import dotenv from "dotenv"
+dotenv.config();
+console.log("MONGODB_URI:", process.env.MONGODB_URI);
+
+
+import mongoose from "mongoose";
+
+export const connectDB = async () => {
+  try {
+    const conn = await mongoose.connect(process.env.MONGODB_URI);
+    console.log(`MongoDB connected: ${conn.connection.host}`);
+  } catch (error) {
+    console.log("MongoDB connection error:", error);
+  }
+};
